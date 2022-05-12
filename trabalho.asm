fd8f5df74bcad9ee7a06b71b1b1242b25c8dd0ad
ThatGuy2001 <rodrigues.diogofilipe@gmail.com>
2022-05-12 23:57:22 +0100

    fuck this shit

--- trabalho.asm
+++ trabalho.asm
@@ -1,25 +1,28 @@
 .globl main
 .data 
 
-fileRGB:	.string "/home/topes1/Documents/trabalho/exemplo1.rgb"  #a string é a localização do ficheiro
-fileGray:	.string "/home/topes1/Documents/trabalho/graytrees.gray" #a string é a localização futura do ficheiro .gray
-FileFinal: 	.string "/home/topes/Documents/trabalho/lines.gray" # o ficheiro final do programa
-buffer_rgb:	.space 30000 #o espaco reserved para a leitura do ficheiro de rgb
-buffer_gray: 	.space 10000 #espaço reservado para o ficheiro .gray
-buffer_gray_horizontal .space 10000 #espaço reservado para a aplicação de sobel horizontal
-buffer_gray_vertical 	.space 10000 #espaço reservado para a aplicação de sobel vertical
-buffer_final 	.space 10000 #espaço reservado para a forma final do ficheiro
+fileRGB:	.string "/home/that_guy/Desktop/trabalho_ac1/lena/lena.rgb"  #a string é a localização do ficheiro
+fileGray:	.string "/home/that_guy/Desktop/trabalho_ac1/lena/teste.gray" #a string é a localização futura do ficheiro .gray
+FileFinal: 	.string "/home/that_guy/Desktop/trabalho_ac1/lena/lines.gray" # o ficheiro final do programa
+buffer_rgb:	.space 786432 #o espaco reserved para a leitura do ficheiro de rgb
+buffer_gray: 	.space 262144 #espaço reservado para o ficheiro .gray
+		.align 2
+buffer_gray_horizontal: .space 262144	#espaço reservado para a aplicação de sobel horizontal
+buffer_gray_vertical: 	.space 262144 	#espaço reservado para a aplicação de sobel vertical
+buffer_final: 	.space 10000 	#espaço reservado para a forma final do ficheiro
 SobelH: 	.word -1,0,1,-2,0,2,-1,0,1
-SobelV:	
+SobelV:		.word -1,-2,-1,0,0,0,1,2,1
 array_somatorio: .space 36 #array com espaço para 9 int
-side_size: 	.word 100 # tamanho do lado, contante por isso fica em data
-one:		.word 1 # constante que pode ser necessaria 
-.text
+side_size: 	.word 512 # tamanho do lado, contante por isso fica em data
+
+filesobelV: 	.string "/home/that_guy/Desktop/trabalho_ac1/lena/sobel.gray"
+
+.text		
 
 main:
 
 ##############################################
-#Fazemos load do adress da string que contem a localização do ficheiro .rgb,
+#Fazemos load do adrress da string que contem a localização do ficheiro .rgb,
 #e do buffer onde vamos ter a imagem antes de a processar para a0 e a1 
 #respetivamente
 #O a2 é aqui declardo para a facil modificação do codigo para outros tamanhos
@@ -28,7 +31,7 @@ main:
 
 	la a0,fileRGB
 	la a1,buffer_rgb
-	li a2,30000 
+	li a2,786432 
 	jal read_rgb_image
 	
 	
@@ -40,7 +43,7 @@ main:
 
 	la a0,buffer_rgb
 	la a1, buffer_gray
-	li a2, 30000
+	li a2, 262144
 	jal rgb_to_gray	
 	
 	
@@ -52,7 +55,7 @@ main:
 
 	la a0, fileGray
 	la a1, buffer_gray
-	li a2, 10000
+	li a2, 262144
 	jal write_gray_image	
 	
 	
@@ -61,15 +64,21 @@ main:
 #argumento os diferentes buffers e matrizes
 ##############################################
 
-
-	la a0,buffer_gray
-	la a1,buffer_gray_horizontal
-	jal convolution
+	la a0 , buffer_gray
+	la a1, SobelH
+	la a2, buffer_gray_horizontal
+	jal convolution # convolution(a0 - imagem .gray, a1- operador de Sobel, a2 - buffer para nova matriz)
 	
-	la a0,buffer_gray
-	la a1,buffer_gray_vertical
-	jal convolution
+	la a0, filesobelV
+	la a1, buffer_gray_horizontal
+	li a2, 260100
+	jal write_gray_image
 	
+	
+	#la a0 , buffer_gray
+	#la a1, SobelV
+	#la a2, buffer_gray_vertical
+	#jal convolution
 	
 ##############################################	
 #Função onde somamos ambas as matrizes com o sobel aplicado seja vertical
@@ -77,17 +86,20 @@ main:
 #
 ##############################################
 	
-	la a0,buffer_gray_horizontal
-	la a1,buffer_gray_vertical
-	la a2,buffer_final
-	li a3,10000
-	jal final_sum
+	#la a0,buffer_gray_horizontal
+	#la a1,buffer_gray_vertical
+	#la a2,buffer_final
+	#li a3,260100
+	#jal final_sum
 	
 	
 ##############################################
 #The end the grand finale. Wow what a ride damn. Well jeez hope it works
 #CPU's are just rocks we fooled into making math for us
 ##############################################
+	
+	
+	
 
 j end
 
@@ -156,14 +168,13 @@ loop: #operacao rgb para gray I = 0.30R + 0.59G + 0.11B.
 																								
 write_gray_image: #### write_gray_image (a0 - string ,a1 - buffer gray, a2 - tamanho) 
 	
-	addi sp, sp, -8  
-	sw a0,4(sp)      
+	addi sp, sp, -4  
 	sw a1, 0(sp)     
 	
 	li a7, 1024    #
 	li a1, 1         #open to write
 	ecall 		   #
-	
+	mv s0, a0
 	 
 	lw a1, 0(sp)	
 	addi sp, sp, 4     
@@ -172,10 +183,10 @@ write_gray_image: #### write_gray_image (a0 - string ,a1 - buffer gray, a2 - tam
 	ecall 			#
 	
 	
-	lw a0,0(sp)
-	addi sp,sp,4
+	
 	
 	li a7, 57		#close file
+	mv a0,s0
 	ecall			#
 
 	ret	
@@ -183,85 +194,118 @@ write_gray_image: #### write_gray_image (a0 - string ,a1 - buffer gray, a2 - tam
 
 convolution: # convolution(a0 - imagem .gray, a1- operador de Sobel, a2 - buffer para nova matriz)
 
+	addi sp, sp, -32
+	sw s1, 0(sp)
+	sw s2, 4(sp)
+	sw s3, 8(sp)
+	sw s4, 12(sp)
+	sw s5, 16(sp)
+	sw s6, 20(sp)
+	sw s7, 24(sp)
+	sw ra, 28(sp)
+	
+	
+	mv s4, a0
+	mv s5, a1
+	mv s6, a2
+	
 	lw s0, side_size
-	addi t0, s0, -1 #vai ser usado para parametro de comparação dos for's
+	addi s7, s0, -1 #vai ser usado para parametro de comparação dos for's
 	
 	
-	li t1, 1 # i em array[i,j]		
+	li s1, 1 # i em array[i,j]		
 		 #inicializados em 1 porque a primira posiçao fora das margens é array[1,1]										
-																																												
+																																													
 for_i: 
-	li t2, 1 # j em array[i,j]
+	li s2, 1 # j em array[i,j]
 	
 for_j:		#{dentro do nosso loop	
  		
- 		mul s1, s0, t1 # efeito de i no movimento do array
- 		addi s1, s1, t2 #somamos o efeito de j no movimento do array
+ 		lw t6 , side_size
+ 		mul s3, t6, s1 	# efeito de i no movimento do array
+ 		add s3, s3, s2 #somamos o efeito de j no movimento do array
  		
- 		li t4, 4 #para multiplicar o incidce de movimento pelos 4  bits
+ 		add t1, s4, s3	# byte em que vamos trabalhar
  		
- 		mul s1, s1, t4 	#movimento a partir de a0 
- 		add t3, a0, s1	#byte de byte em que vamos trabalho 
+ 		mv a0, t1
+ 		mv a1, s5
  		
+ 		jal sobel
  		
+ 		sb a0, 0(s6)
+ 		addi s6, s6, 1
  		
- 		
- 
  		#}
-  		addi t2, t2, 1
-  		blt t2, t0, for_j
-	addi t1, t1, 1
-		
-    	blt t1, t0, for_i 
+ 		
+  		addi s2, s2, 1	  	# incrementação  de for_j	
+  		blt s2, s7, for_j 	# condição de for_j
+  		
+  		
+	addi s1, s1, 1		# incrementação  de for_i
+    	blt s1, s7, for_i 	# condição de for_i
+    	
+    	
+    	lw s1, 0(sp)
+	lw s2, 4(sp)
+	lw s3, 8(sp)
+	lw s4, 12(sp)
+	lw s5, 16(sp)
+	lw s6, 20(sp)
+	lw s7, 24(sp)
+	lw ra, 28(sp)
+	addi sp, sp, 32
 
-final_sum:###final_sum(a0-buffer com o sobel horizontal;a1-buffer com o sobel vertical;a2-buffer final;a3-tamanho para criar) 
-	addi sp,sp,4
-	sw ra,0(sp)
+	ret 
 	
-	lbu t0, 0(a0) 
+final_sum:###final_sum(a0-buffer com o sobel horizontal;a1-buffer com o sobel vertical;a2-buffer final;a3-tamanho para criar) 
+	
+	
+loop4:	lbu t0, 0(a0) 
 	lbu t1, 0(a1) 
 	
-	add t2,t2,t0
-	add t2,t2,t1
+	add t2,t0,t1
+	srli t2, t2, 1
+	sb t2, 0(a2)
 	
 	addi a0,a0,1
 	addi a1,a1,1
 	addi a2,a2,1
 	addi a3,a3,-1
 	
-	bnez a3,final_sum
+	bnez a3,loop4
 	
+
 	#open to write
 	
 	li a7, 1024
 	la a0, FileFinal
 	li a1, 1
 	ecall
-	
+	mv s0,a0
 	#write 
 	
 	li a7, 64
 	la a1,buffer_final 
-	li a2,10000	
+	li a2,260100
 	ecall 
 	
 	#close file
 	
-	lw a0,0(sp)
-	addi sp,sp,4
 	
+	mv a0, s0
 	li a7, 57
 	ecall	
 							
 								
 											
-																																																														
-end:
+	ret
 
 
-sobel: 	#nem sei bem dizer o que isto faz, mas isto faz o que isto faz  
-	#recebe um operador se sobel, e o bit que quermos trabalhar na matriz e retorna o resultado da convolution para o A[i,j]
+sobel: 	#recebe um operador se sobel, e o bit que quermos trabalhar na matriz e retorna o resultado da convolution para o A[i,j]
 	# (a0- byte de trabalho,  a1- sobel )
+	
+	addi sp, sp ,-4
+	sw ra, 0(sp)
 	
 	la a2, array_somatorio
 	#vamos percorrer os valores -1,0,1 para i e j 
@@ -275,22 +319,19 @@ for_j2:
 	#{dentro do nosso loop	
 	
 	mul t2 , t5, t0 # efeito de i no movimento do array
- 	addi t2, t2, t1 #somamos o efeito de j no movimento do array
- 		
- 	li t4, 4 #para multiplicar o incidce de movimento pelos 4  bits
- 		
- 	mul t2, t2, t4 	#movimento a partir de a0 
+ 	add t2, t2, t1 #somamos o efeito de j no movimento do array
+ 		 
  	add t2, a0, t2
  	
- 	lw t2, 0(t2)
+ 	lbu t2, 0(t2)
  	lw t3, 0(a1)
 	 			
 	mul t2, t2, t3
 	
 	sw t2, 0(a2)
 	
-	addi a2, a2, 1
-	addi a1, a1, 1
+	addi a2, a2, 4
+	addi a1, a1, 4
 	
 	#}
 	addi t1, t1, 1
@@ -306,22 +347,34 @@ for_j2:
 	jal soma_array
 	
 	
+	
+	lw ra, 0(sp)
+	addi sp, sp, 4
+		
+	ret
+	
 soma_array: #(a0 - array, a1- sz)
 
 	li t0, 0
 	
-loop1: lw t1, 0(a0)
+loop1:  lw t1, 0(a0)
 	add t0, t0, t1
 	addi a1, a1, -1
-	addi a0, a0, 1
+	addi a0, a0, 4
 	
-	bgtz a0, loopl
+	bgtz a1, loop1
 	
-	mv a0, t0
+	bgez t0, R
+		not t0,t0
+		addi t0,t0,1
+		
+R:	mv a0, t0
 	ret
 
 	
 	
 	
 	
-	
+
+																																																														
+end:
